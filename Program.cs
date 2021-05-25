77dde74ddcac407872a65a198b5742a545a77318
Autumn Fotopoulos <acarte94@gmail.com>
2021-05-25 15:08:57 -0500

    phase1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿// Display a message to the user asking them to guess the secret number.
+// Display a prompt for the user's guess.
+// Take the user's guess as input and save it as a variable.
+// Display the user's guess back to the screen.
 
-namespace GuessingGame
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+using System;
+
+Console.WriteLine("Want to guess my secret number?");
+Console.WriteLine("What is your guess?");
+
+string guess = Console.ReadLine();
+
+Console.WriteLine($"You just guessed {guess}");
